5fcf3876b68489970523cfb09e9b33b8e34d4034
Luerfel <mluerfel@gmail.com>
2024-03-04 11:24:35 -0300

    tratamento de erro

--- adivinhar_numero.py
+++ adivinhar_numero.py
@@ -8,7 +8,12 @@ print("ADIVINHA O NUMERO DE 0 A 100 !!!")
 
 acertou = 0
 while(acertou == 0):
-    numero_jogador = int(input("Digite um Numero : "))
+    chave_numero_correto = 0
+    while (chave_numero_correto == 0):
+        try:
+            numero_jogador = int(input("Digite um Numero : "))
+        except:
+            print("Por favor digite apenas numeros!")        
     if (numero_jogador > numero_aleatorio):
         print("O Número digitado é maior que o número misterioso!!")
     elif (numero_jogador < numero_aleatorio):
