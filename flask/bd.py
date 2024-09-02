4454b0cd1efa400b913cedc2bd2d8dcb6f78bb3c
luerfel <mluerfel@gmail.com>
2024-09-02 16:57:17 -0300

    exercicio 2

--- flask/bd.py
+++
@@ -1,8 +0,0 @@
-Carros = [
-    {
-        'id' : 1,
-        'marca': 'fiat',
-        'modelo' : 'Marea',
-        'ano' : 1999.
-    }
-]
--- flask/main.py
+++ flask/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
-from bd import Carros
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+frutas = ["maçã", "banana", "laranja", "melancia", "uva", "mamão", "abacaxi"]
 
-def get_carros():
-    return Carros
+@app.route('/')
 
-app.run()
+def home_page():
+
+    return render_template("index.html", frutas_do_html=frutas)
+
+
+@app.route('/pagina2')
+def pagina2():
+    return render_template('pagina2.html')
+@app.route('/processar_fruta', methods=['POST'])
+def processar_fruta():
+    nome_da_fruta = request.form['nome']
+    frutas.append(nome_da_fruta)
+    return render_template("pagina2.html", frutas_do_html=frutas)
+
+if __name__ == '__main__':
+    app.run(debug=True)
